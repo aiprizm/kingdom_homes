a3fe734be70b112e4b23f0b5818c453ad5d8edbb
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-09-13 00:40:13 +0530

    enabled sitemap

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.contrib.sitemaps.views import sitemap
+from website.sitemaps import Static_Sitemap
 from . import views
 
 urlpatterns = [
@@ -8,6 +9,8 @@ urlpatterns = [
     path('booking/', views.booking, name='booking'),
     path('book-with-us/', views.booking_page, name='booking_page'),
     path('inner-page/', views.inner_page, name='inner_page'),
+    path('sitemap.xml', sitemap, {'sitemaps': {"static": Static_Sitemap}},
+         name='django.contrib.sitemaps.views.sitemap')
     # path('privacy-policy/', views.privacy, name='privacy'),
     # path('disclaimer/', views.disclaimer, name='disclaimer'),
 ]
